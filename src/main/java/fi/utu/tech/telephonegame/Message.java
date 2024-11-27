a4ae47fd4096e4b18e9fc3b69d8fa5520bee2b10
robi <rnjoha@utu.fi>
2024-11-28 00:44:45 +0200

    Message.java muokattu

--- src/main/java/fi/utu/tech/telephonegame/Message.java
+++ src/main/java/fi/utu/tech/telephonegame/Message.java
@@ -5,21 +5,21 @@ import java.io.Serializable;
 import java.util.UUID;
 
 /**
- * 
+ *
  * Message objects should carry information about the message contents being
  * sent "the actual message" (string) as well as the current amount of hops
  * (the amount of nodes the message has already visited) and an identifier (UUID)
  * to identify the message even if its string content was modified by other nodes.
- * 
- * 
+ *
+ *
  * The message objects will be sent to other nodes by using ObjectStreams
- * 
+ *
  * You'll need to make changes to this class to:
- * 
+ *
  * 1. Make it possible to be sent over network (ie. it is serializable for Java Object Streams)
  * 2. It contains the required fields (UUID id, String message and Integer hops)
  * 3. You are able to access and update the aforementioned attributes to "Refine" them
- * 
+ *
  */
 
 public final class Message implements Serializable {
